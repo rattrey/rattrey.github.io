8b78f7f0745db31e42c82a9c9ebfa47e6d03f10c
Rohin Attrey <attrey.rohin@gmail.com>
2023-12-29 16:51:33 -0500

    Project Card Descriptions
    updated images for key projects using adobe stock
    -updated css to allow overflow of text in p tag of project-info
    -created detailed descriptions for each project

--- visualizations.py
+++ visualizations.py
@@ -1,69 +1,33 @@
-import plotly.graph_objects as go
+import matplotlib.pyplot as plt
 import numpy as np
 
-# Constants for the multimodal 3D function
-a, d, f = 0.05, 0.03, 0.04
-b, e = 50, 20  # Optimal price point and seasonal/store factor
-c = 1500  # Max sales/margin
-g, h, i = 10, 15, 5  # Additional factors for complexity
+# Creating data for supply and demand curves
+quantity = np.arange(0, 100)
+demand = 100 - quantity  # Example demand curve
+supply = quantity  # Example supply curve
 
-# Generating Price and Seasonal/Store Size Factor Data
-price = np.linspace(0, 100, 100)
-seasonal_factor = np.linspace(0, 40, 100)
-price_grid, seasonal_factor_grid = np.meshgrid(price, seasonal_factor)
+# Finding the equilibrium point
+equilibrium_price = np.argwhere(np.diff(np.sign(demand - supply)) != 0).reshape(-1) + 0
+equilibrium_quantity = quantity[equilibrium_price]
 
-# Multimodal and complex calculations for sales/margin
-sales = (-a * np.cos(price_grid / g) * np.cos(seasonal_factor_grid / h)
-         - d * (price_grid - b)**2 
-         - f * (seasonal_factor_grid - e)**2 
-         + c
-         + 300 * np.exp(-((price_grid - 30)**2 + (seasonal_factor_grid - 10)**2) / i**2)
-         + 200 * np.exp(-((price_grid - 70)**2 + (seasonal_factor_grid - 30)**2) / i**2))
+# Creating the plot
+plt.figure(figsize=(10, 6))
+plt.plot(quantity, demand, label='Demand Curve', color='blue')
+plt.plot(quantity, supply, label='Supply Curve', color='green')
 
-# Creating the 3D plot with enhanced grid lines
-fig = go.Figure(data=[go.Surface(z=sales, x=price_grid, y=seasonal_factor_grid, colorscale='Cividis')])
+# Annotating the equilibrium point
+plt.scatter(equilibrium_quantity, equilibrium_price, color='red')
+plt.text(equilibrium_quantity, equilibrium_price, f'  Equilibrium Point ({equilibrium_quantity[0]}, {equilibrium_price[0]})', 
+         verticalalignment='bottom', horizontalalignment='left')
 
-# Customize grid lines
-fig.update_layout(
-    scene=dict(
-        xaxis=dict(showticklabels=False, title='', gridcolor='lightgrey', zerolinecolor='grey'),
-        yaxis=dict(showticklabels=False, title='', gridcolor='lightgrey', zerolinecolor='grey'),
-        zaxis=dict(showticklabels=False, title='', gridcolor='lightgrey', zerolinecolor='grey')),
-    autosize=False,
-    width=800, height=800,
-    margin=dict(l=65, r=50, b=65, t=90))
+# Setting the title and labels
+plt.title('Supply and Demand Curve')
+plt.xlabel('Quantity')
+plt.ylabel('Price')
+plt.legend()
 
-#Causal impact
-import plotly.graph_objects as go
-from plotly.subplots import make_subplots
-import numpy as np
-import pandas as pd
+# Adding grid for better readability
+plt.grid(True)
 
-# Generate synthetic data for illustration
-np.random.seed(42)
-pre_period_data = np.random.normal(100, 10, 120)
-post_period_data = np.random.normal(120, 10, 60)
-data = np.concatenate((pre_period_data, post_period_data))
-dates = pd.date_range(start='2020-01-01', periods=180, freq='D')
-
-# Intervention index for when the change occurred
-intervention_index = 120
-
-# Create subplots
-fig = make_subplots(rows=3, cols=1, shared_xaxes=True, vertical_spacing=0.02)
-
-# Panel 1: Actual vs Predicted
-fig.add_trace(go.Scatter(x=dates, y=data, mode='lines', name='Observed'), row=1, col=1)
-fig.add_trace(go.Scatter(x=dates[intervention_index:], y=data[intervention_index:], mode='lines', name='Predicted'), row=1, col=1)
-
-# Panel 2: Pointwise effects (difference between actual and predicted)
-point_effects = data[intervention_index:] - data[intervention_index]
-fig.add_trace(go.Scatter(x=dates[intervention_index:], y=point_effects, fill='tozeroy', name='Point Effects'), row=2, col=1)
-
-# Panel 3: Cumulative effect
-cumulative_effect = np.cumsum(point_effects)
-fig.add_trace(go.Scatter(x=dates[intervention_index:], y=cumulative_effect, fill='tozeroy', name='Cumulative Effect'), row=3, col=1)
-
-# Update layout
-fig.update_layout(height=600, width=800, title_text="Causal Impact Analysis Visualization")
-fig.show()
+# Showing the plot
+plt.show()
